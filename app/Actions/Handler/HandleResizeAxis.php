8e9af03310673b7d96afa63809da46af2c3be3f4
Alex Roth <psion@tuta.io>
2024-10-16 11:17:56 +0200

    feat: handle resize axis

---
+++ app/Actions/Handler/HandleResizeAxis.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Handler;
+
+use App\DTOs\Request;
+use ReflectionMethod;
+use Spatie\Image\Image;
+use App\Services\Metrics;
+use App\Contracts\Actions\SavesImage;
+
+use function Laravel\Prompts\progress;
+
+final class HandleResizeAxis
+{
+    private Request $request;
+
+    public function __construct(private SavesImage $save) {}
+
+    public function __invoke(Request $request): Metrics
+    {
+        $this->request = $request;
+        unset($request);
+
+        $progress = fn () => progress(
+            label: 'Processing images...',
+            steps: $this->request->images,
+            callback: function (Image $image) {
+                $this->applyResizeAxis($image);
+                ($this->save)($image, $this->request->target, $this->request->format);
+            },
+        );
+
+        return new Metrics($progress, $this->request->images->count());
+    }
+
+    private function applyResizeAxis(Image $image): void
+    {
+        assert($this->request->axis instanceof ReflectionMethod);
+
+        $this->request->axis->invoke($image, $this->request->value);
+    }
+}
