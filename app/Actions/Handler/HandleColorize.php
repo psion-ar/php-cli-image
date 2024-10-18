1b6be2620a94b84308a401b5351cefab1998c7bf
Alex Roth <psion@tuta.io>
2024-10-18 12:11:05 +0200

    feat: handle colorize

---
+++ app/Actions/Handler/HandleColorize.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Handler;
+
+use App\Contracts\Actions\SavesImage;
+use App\DTOs\Request;
+use App\Services\Metrics;
+use Spatie\Image\Image;
+
+use function Laravel\Prompts\progress;
+
+final class HandleColorize
+{
+    private Request $request;
+
+    public function __construct(private SavesImage $save) {}
+
+    public function __invoke(Request $request): Metrics
+    {
+        $this->request = $request;
+        unset($request);
+
+        $progress = fn () => progress(
+            label: 'Processing images...',
+            steps: $this->request->images,
+            callback: function (Image $image) {
+                $this->applyColorize($image);
+                ($this->save)($image, $this->request->target, $this->request->format);
+            },
+        );
+
+        return new Metrics($progress, $this->request->images->count());
+    }
+
+    private function applyColorize(Image $image): void
+    {
+        $image->colorize($this->request->red, $this->request->green, $this->request->blue);
+    }
+}
